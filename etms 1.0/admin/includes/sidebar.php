0f1c931d1243b23bbaa08481ed61d97446e6efc2
deeniesh21 <71019867+deeniesh21@users.noreply.github.com>
2020-09-20 13:46:33 +0800

    ETMS_v1.0

---
+++ etms 1.0/admin/includes/sidebar.php
@@ -0,0 +1,69 @@
+     <aside id="slide-out" class="side-nav lime fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Training Type<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addtrainingtype.php">Add Training Type</a></li>
+                                <li><a href="managetrainingtype.php">Manage Training Type</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Employees<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Add Employee</a></li>
+                                <li><a href="addtr.php">Add Trainer</a></li>
+                                <li><a href="addhr.php">Add Human Resource Manager</a></li>
+                                <li><a href="addcm.php">Add Cash Manager</a></li>
+                                <li><a href="manageemployee.php">Manage Employee</a></li>
+                                <li><a href="managetr.php">Manage Trainer</a></li>
+                                <li><a href="managehr.php">Manage Human Resources Manager</a></li>
+                                <li><a href="managecm.php">Manage Cash Manager</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Training Management<i class="nav-drop-icon material-icons"></i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="training.php">All Training </a></li>
+                                <li><a href="pendingtraininghistory.php">Pending Training </a></li>
+                                <li><a href="approvedtraininghistory.php">Approved Training</a></li>
+                                  <li><a href="notapprovedtraining.php">Not Approved Training</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+  <li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons">settings_input_svideo</i>Change Password</a></li>
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+     
+                </div>
+            </aside>
